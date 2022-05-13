244fb94c73914a8f0920a714d92dfffe14956605
Dean Chérif <dcherif@segepar-pharma.com>
2022-05-13 15:17:42 +0200

    Fix routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import LoadingOrError from 'components/LoadingOrError'
 import { ReactElement } from 'react'
 import { lazy, Suspense } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 const AtlasSkillTree = lazy(async () => import('pages/AtlasSkillTree'))
 
 const App = (): ReactElement => (
   <HashRouter>
-    <Routes basename='/'>
+    <Routes>
+      <Route path='/' element={<Navigate to={'/tree'} />} />
       <Route
-        path=':tree'
+        path='/tree'
+        element={
+          <Suspense fallback={<LoadingOrError />}>
+            <AtlasSkillTree />
+          </Suspense>
+        }
+      />
+      <Route
+        path='/tree/:tree'
         element={
           <Suspense fallback={<LoadingOrError />}>
             <AtlasSkillTree />
